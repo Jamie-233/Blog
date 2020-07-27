dfee92f0d52f65786b50f7dc298e55bef881e6a3
jiancun <jiancun.zhao@70capital.com>
2020-07-27 11:01:12 +0800

    modify Gemfile

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :update, :edit, :destroy]
+  before_action :authenticate_admin!, except: [:index, :show]
 
   def index
     @categories = Category.all
