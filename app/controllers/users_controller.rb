eb495d124f044f4ebe4f08e336d4cdaeadf4ebae
jiancun <jiancun.zhao@70capital.com>
2020-07-31 15:49:22 +0800

    modify style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @users = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] =  "create success"
+    end
+  end
+
+  private
+
+  def user_params
+    params.reuire(:user).permit(:name :email)
   end
 end
