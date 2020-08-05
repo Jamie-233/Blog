9eae6f44326bffd9e99a20853eda29be4be548f4
jiancun <jiancun.zhao@70capital.com>
2020-08-05 18:30:37 +0800

    Finsh implementation of blog functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
   def full_title(page_title = '')
-    base_title = 'Blog'
+    base_title = '简存 - 创作我的创作'
+    simple_title = '简存'
     if page_title.empty?
       base_title
     else
-      page_title + " | " + base_title
+      page_title + " - " + simple_title
     end
   end
 end
