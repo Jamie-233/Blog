73f40fb36da591fa501e4a0b64c5d717a4ee8e36
jiancun <jiancun.zhao@70capital.com>
2020-08-06 15:06:56 +0800

    modify comment redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,8 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     # @comment = @post.comments.create(params[:comment].permit(:name, :comment))
-
     @comment = @post.comments.create(params.require(:comment).permit(:name, :comment))
-    
-
+    redirect_to post_path(@post)
   end
 
   def destroy
