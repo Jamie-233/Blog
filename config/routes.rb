a9ea9e0e4a201627e9e5b1be4523f4c718d10202
jiancun <jiancun.zhao@70capital.com>
2020-08-24 11:14:47 +0800

    add user  authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  before_action :authenticate_user!, except: [:index, :show]
 
   root "posts#index"
-  devise_for :admins
+  devise_for :admin
   resources :categories
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    # get 'users/new'
